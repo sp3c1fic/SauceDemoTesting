c8d1fb424259335c50b832ef84512636da326a36
Димитър Димитров <dafgasg94@gmail.com>
2026-03-24 18:18:12 +0100

    Initial Commit

---
+++ SauceDemo/Driver.cs
@@ -0,0 +1,6 @@
+﻿namespace SauceDemo
+{
+    public class Driver
+    {
+    }
+}
---
+++ SauceDemoTests/Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace SauceDemoTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
